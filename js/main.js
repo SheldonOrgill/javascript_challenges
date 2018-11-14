77826b39f8d74ecebbe341bd674298a8c692fd37
SheldonOrgill <S.orgill@live.co.za>
2018-11-14 16:21:50 +0200

    Challenge 1 has been completed with all the different type of data types.

---
+++ js/main.js
@@ -0,0 +1,51 @@
+// ARRAY
+let fruits = ["Apple", "Orange", "Plum"];
+
+console.log( fruits[0] ); // Apple
+console.log( fruits[1] ); // Orange
+console.log( fruits[2] ); // Plum
+
+// OBJECT
+
+var student = 'Sheldon orgill';
+    age = 24;
+    course = 'coding course';
+    address = '11A Monty Road Southfield';
+
+    console.log("I am " + student +" i am " + age + " and i live at "+ address +" i am currently doing a "+ course + ".");
+
+    // STRINGS
+
+    // let single = 'single-quoted';
+    // let double = "double-quoted";
+    // let backticks = `backticks`;
+
+    var name = 'Sheldon';
+        age = '24';
+        gender = 'male';
+    console.log(name);
+
+    // NUMBER
+
+    let x = 22;
+        console.log(x); 
+
+    // BOOLEAN
+
+    let greater = 10 > 3 ;
+    console.log(greater); 
+        
+    //   NULL
+        
+        let color = null;
+        console.log(color);   
+        
+    // UNDEFINED
+
+        let empty;
+        console.log(empty);
+    
+     //DATE
+
+        let theDate = new Date();
+        console.log(theDate);      
